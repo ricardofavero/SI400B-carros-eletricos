560bbc3ceab32aa59e28d7c12204a827ba83655b
isadorafonseca <Isadora@Isadoras-MBP.home>
2016-08-29 20:56:17 -0300

    classe endereco

---
+++ RecargaCarro/src/recargacarro/Endereco.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recargacarro;
+
+/**
+ *
+ * @author Acer
+ */
+public class Endereco {
+    private String rua;
+    private int numero;
+    private String cidade;
+    private String estado;
+    private int zip;
+}
