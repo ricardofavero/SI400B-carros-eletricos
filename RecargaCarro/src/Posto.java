1e33b9b6218248e514713d82183e83de4157eb1a
FernandaGravena <fernandamgravena@gmail.com>
2016-08-30 00:06:44 -0300

    classe posto

---
+++ RecargaCarro/src/Posto.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recargacarro;
+
+/**
+ *
+ * @author Acer
+ */
+public class Posto {
+    private String nome;
+    private Endereco endereco;
+    private String telefone;
+    private Localizacao localizacao;
+    //é possivel calcular a distancia entre pontos lat-long por meio de uma formula!
+    private String horarioFunc;
+    private String observacao;
+    
+    void exibir(){
+    }
+}
